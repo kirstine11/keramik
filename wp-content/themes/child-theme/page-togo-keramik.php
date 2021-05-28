ec524f20c68768674233431af6cd2ce17a2373a1
Julie Eggertsen <Julieeggertsen1@Christinas-MacBook-Pro.local>
2021-05-28 17:58:26 +0200

    Tilføjelse af filtrering på to go keramik side

--- wp-content/themes/child-theme/page-togo-keramik.php
+++ wp-content/themes/child-theme/page-togo-keramik.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying front page.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -13,32 +8,116 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
+get_header();
+?>
 
-get_header(); ?>
+<template>
+    <article class="loopart">
+        <img src="" alt="keramikpakke_billede" class="keramikpakkeimg">
+        <h2 class="pakkenr"></h2>
+        <div class="tekst">
+            <h2 class="titel"></h2>
+            <p class="beskrivelse"></p>
+            <p class="pris"></p>
+            <button class="read_more">Læs mere</button>
+        </div>
+    </article>
+</template>
 
-<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <div>
+            <!--
+            <div id="podHead">
+                <h2>PODCAST</h2>
+                <h6>Find vores mange programmer, udsendelser, serier, afsnit og episoder her.
+                </h6>
+                <img src="http://julieeggertsen.dk/kea/2_sem/tema_09/09_loud/09_loud_site/wp-content/uploads/2021/04/people_train.jpg" alt="train_boy">
+            </div>
+-->
+            <nav id="filtrering"><button data-keramikpakke="alle">Alle</button> <button data-keramikpakke="6"> Krus </button>
+                <button data-keramikpakke="5"> <img src=""> Vase </button>
+                <button data-keramikpakke="13"> <img src=""> Skål </button>
+                <button data-keramikpakke="14"> <img src=""> Fad </button>
+                <button data-keramikpakke="15"> <img src=""> Figur </button>
+                <button data-keramikpakke="16"> <img src=""> Andet </button>
+            </nav>
 
-	<?php get_sidebar(); ?>
+        </div>
 
-<?php endif ?>
+        <section id="keramikcontainer">
+        </section>
+    </main> <!-- #main -->
+</section> <!-- #primary -->
 
-	<div id="primary" <?php astra_primary_class(); ?>>
 
-		<?php astra_primary_content_top(); ?>
+<script>
+    let keramikpakke;
+    let indhold;
+    let filterKeramikpakke = "alle";
+    const dbUrl = "http://kirstinekrogs.dk/kea/eksamen_kreamik/wp-json/wp/v2/keramikpakke?per_page=100";
+    const catUrl = "http://kirstinekrogs.dk/kea/eksamen_kreamik/wp-json/wp/v2/indhold";
 
-		<?php astra_content_page_loop(); ?>
+    async function getJson() {
+        const data = await fetch("http://kirstinekrogs.dk/kea/eksamen_kreamik/wp-json/wp/v2/keramikpakke?per_page=100");
+        const catdata = await fetch("http://kirstinekrogs.dk/kea/eksamen_kreamik/wp-json/wp/v2/indhold");
+        keramikpakke = await data.json();
+        indhold = await catdata.json();
+        console.log(indhold);
+        visKeramikpakker();
+        opretknapper();
+    };
 
-		<?php astra_primary_content_bottom(); ?>
 
-	</div><!-- #primary -->
+    function opretknapper() {
+        indhold.forEach(cat => {
+            document.querySelector("#filtrering").innerHTML
+        })
+        addEventListenerToButtons();
+    }
 
-<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+    function addEventListenerToButtons() {
+        document.querySelectorAll("#filtrering button").forEach(elm => {
+            elm.addEventListener("click", filtrering);
+        })
+    };
 
-	<?php get_sidebar(); ?>
 
-<?php endif ?>
 
-<?php get_footer(); ?>
+    function filtrering() {
+        filterKeramikpakke = this.dataset.keramikpakke;
+        console.log(filterKeramikpakke);
+        visKeramikpakker();
+
+    }
+
+
+
+
+    function visKeramikpakker() {
+        let temp = document.querySelector("template");
+        let container = document.querySelector("#keramikcontainer")
+        container.innerHTML = "";
+        keramikpakke.forEach(keramikpakke => {
+            if (filterKeramikpakke == "alle" || keramikpakke.indhold.includes(parseInt(filterKeramikpakke))) {
+
+                let klon = temp.cloneNode(true).content;
+                klon.querySelector(".keramikpakkeimg").src = keramikpakke.billede.guid;
+                klon.querySelector("h2").textContent = keramikpakke.title.rendered;
+                //                klon.querySelector(".beskrivelse").textContent = keramikpakke.beskrivelse;
+                klon.querySelector(".pris").textContent = keramikpakke.pris;
+                klon.querySelector(".read_more").addEventListener("click", () => {
+                    location.href = keramikpakke.link;
+                })
+                container.appendChild(klon);
+            }
+        })
+    }
+
+
+
+    getJson();
+
+</script>
+<?php
+get_footer();
